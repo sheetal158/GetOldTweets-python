5642d6d47dea7bd2f0196c16a9fb6015dfd5a582
Sheetal Pandrekar <sheetalpandrekar@Sheetal-Mac.local>
2017-09-19 12:13:23 -0400

    All Opioids

--- Main.py
+++ Main.py
@@ -1,8 +1,34 @@
-import sys
+import sys,getopt,datetime,codecs
+import re
+
 if sys.version_info[0] < 3:
     import got
 else:
     import got3 as got
+    
+def cleanText(text):
+
+	text = text.replace(","," ").replace("\n", " ").replace("\r", " ").replace("\r\n", " ")
+	filtered_text = ""
+    
+	words = text.split(" ")
+	for i in range(0,len(words)):
+		words[i] = words[i].strip().lower()
+        
+		try:
+			if words[i].startswith('http'):
+				words[i] = ""
+		except:
+			pass
+
+	text = " ".join(words)
+	
+	filtered_text = text
+    
+    #keep only informative characters
+	#filtered_text = re.sub('[^a-z|A-Z|0-9|\s|\.|@]', '', text);
+    
+	return filtered_text
 
 def main():
 
@@ -13,24 +39,36 @@ def main():
 		print("Text: %s" % t.text)
 		print("Mentions: %s" % t.mentions)
 		print("Hashtags: %s\n" % t.hashtags)
+	
+	
+	outputFileName = "output_got.csv"				
+	outputFile = codecs.open(outputFileName, "a", "utf-8")
+	dateSince_str = "2016-09-14"
+	dateUntil_str = "2017-09-16"
+	dateFinal_str = dateUntil_str
+	
+	#outputFile.write('username,date,retweets,favorites,text,geo,mentions,hashtags,id,permalink')
 
-	# Example 1 - Get tweets by username
-	tweetCriteria = got.manager.TweetCriteria().setUsername('barackobama').setMaxTweets(1)
-	tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
+	print('Searching...\n')
+	def receiveBuffer(tweets):
+			
+		for t in tweets:
+			outputFile.write(('\n%s,%s,%d,%d,%s,%s,%s,%s,%s' % (t.username,t.date.strftime("%Y-%m-%d"), int(t.retweets), int(t.favorites), cleanText(t.text), str(t.geo), str(t.mentions), str(t.hashtags), str("id"+t.id))))
+		outputFile.flush();
+		print('More %d saved on file...\n' % len(tweets))
+		
+	
+	dateFinal = datetime.datetime.strptime(dateFinal_str, "%Y-%m-%d")
+	dateSince = datetime.datetime.strptime(dateSince_str, "%Y-%m-%d")
 
-	printTweet("### Example 1 - Get tweets by username [barackobama]", tweet)
+	while dateFinal>dateSince:
+		tweetCriteria = got.manager.TweetCriteria().setQuerySearch('acetaminophen OR analgesics OR antipyretic OR duragesic OR durogesic OR fentanyl OR heroin OR hydrocodine OR hydrocodone OR hydromorphone OR hydros OR ibuprofen OR lortab OR methadone OR morphine OR narcotic OR narcotics OR opiate OR opioid OR opium OR oxycodone OR oxycontin OR oxycottin OR oxycotton OR percodan OR tylenol OR vicodin OR vicoprofen OR xanax').setSince(dateSince_str).setUntil(dateFinal_str)
+		#tweetCriteria = got.manager.TweetCriteria().setQuerySearch('fentanyl').setSince(dateSince_str).setUntil(dateFinal_str)
+		tweets = got.manager.TweetManager.getTweets(tweetCriteria, receiveBuffer)
+		print('Switching...\n')
+		dateFinal_str = tweets[-1].date.strftime("%Y-%m-%d")
+		dateFinal = datetime.datetime.strptime(dateFinal_str, "%Y-%m-%d")
 
-	# Example 2 - Get tweets by query search
-	tweetCriteria = got.manager.TweetCriteria().setQuerySearch('europe refugees').setSince("2015-05-01").setUntil("2015-09-30").setMaxTweets(1)
-	tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
-
-	printTweet("### Example 2 - Get tweets by query search [europe refugees]", tweet)
-
-	# Example 3 - Get tweets by username and bound dates
-	tweetCriteria = got.manager.TweetCriteria().setUsername("barackobama").setSince("2015-09-10").setUntil("2015-09-12").setMaxTweets(1)
-	tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
-
-	printTweet("### Example 3 - Get tweets by username and bound dates [barackobama, '2015-09-10', '2015-09-12']", tweet)
 
 if __name__ == '__main__':
 	main()
